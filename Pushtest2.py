f028928285d87a1afbb6e515447184903605fc4f
Projectduebonn <euphrosye@gmail.com>
2023-02-27 14:33:16 +0100

    Main

--- Pushtest2.py
+++ Pushtest2.py
@@ -3,3 +3,7 @@ import random
 for i in range(10):
     print(random.randint(1,100))
     
+
+#print a list of 10 random numbers between 1 and 100
+import random
+print([random.randint(1,100) for i in range(10)])
